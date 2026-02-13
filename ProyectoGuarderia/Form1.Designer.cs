18141843be78754fa8e19345fa0f48799d9b9f2f
Estereo133 <l23070019@huetamo.tecnm.mx>
2026-02-13 17:57:39 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoGuarderia/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ProyectoGuarderia
+{
+    partial class Form_Menu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Menu = new System.Windows.Forms.Label();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lbl_Menu
+            // 
+            this.lbl_Menu.AutoSize = true;
+            this.lbl_Menu.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Menu.Location = new System.Drawing.Point(-2, -3);
+            this.lbl_Menu.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.lbl_Menu.Name = "lbl_Menu";
+            this.lbl_Menu.Size = new System.Drawing.Size(219, 82);
+            this.lbl_Menu.TabIndex = 0;
+            this.lbl_Menu.Text = "Menu";
+            this.lbl_Menu.Click += new System.EventHandler(this.lbl_Menu_Click);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(359, 1050);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // Form_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(29F, 55F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.lbl_Menu);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(10, 8, 10, 8);
+            this.MaximizeBox = false;
+            this.Name = "Form_Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Menu;
+        private System.Windows.Forms.Panel panelMenu;
+    }
+}
+
---
+++ ProyectoGuarderia/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoGuarderia
+{
+    public partial class Form_Menu : Form
+    {
+        public Form_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_Menu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
